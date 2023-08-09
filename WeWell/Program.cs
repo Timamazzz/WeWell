a6d87ba3956b0fdda6df619cc22cfe6192dbd0d7
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-08-09 10:32:36 +0300

    Add swagger to prod

--- WeWell/Program.cs
+++ WeWell/Program.cs
@@ -59,12 +59,9 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowLocalhost");
 
