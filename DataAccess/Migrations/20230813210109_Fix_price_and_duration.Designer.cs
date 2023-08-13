c9d0d2b8bb8140c9ececd59b05e59c6bb0af7e30
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-08-14 00:02:20 +0300

    add migration

--- DataAccess/Migrations/20230813210109_Fix_price_and_duration.Designer.cs
+++ DataAccess/Migrations/20230813210109_Fix_price_and_duration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230813210109_Fix_price_and_duration")]
+    partial class Fix_price_and_duration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20230813210109_Fix_price_and_duration.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_price_and_duration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adres",
+                table: "Places");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Places",
+                newName: "Address");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Meetings",
+                newName: "MinPrice");
+
+            migrationBuilder.RenameColumn(
+                name: "Duration",
+                table: "Meetings",
+                newName: "MinDurationHours");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxDurationHours",
+                table: "Places",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxPrice",
+                table: "Places",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinDurationHours",
+                table: "Places",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinPrice",
+                table: "Places",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxDurationHours",
+                table: "Meetings",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxPrice",
+                table: "Meetings",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxDurationHours",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "MaxPrice",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "MinDurationHours",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "MinPrice",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "MaxDurationHours",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "MaxPrice",
+                table: "Meetings");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Places",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "MinPrice",
+                table: "Meetings",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "MinDurationHours",
+                table: "Meetings",
+                newName: "Duration");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Adres",
+                table: "Places",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
