8f93e9e9ac686efb81ed1b01bc8cd034ad89928c
Timama <timforworking@mail.ru>
2023-09-17 21:51:30 +0300

    Add Description in serializer

--- WeWell/Models/Places/PlaceGet.cs
+++ WeWell/Models/Places/PlaceGet.cs
@@ -5,6 +5,7 @@ public class PlaceGet
     public int? Id { get; set; }
     public string? Name { get; set; }
     public string? Address { get; set; }
+    public string? Description { get; set; }
     public string? ImagePath { get; set; }
     public string? Url { get; set; }
     public int? MinPrice { get; set; }
