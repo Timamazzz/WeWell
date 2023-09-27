2dd96245c39550d950f1dd84752824896bc9a8d4
Timama <timforworking@mail.ru>
2023-09-27 21:12:02 +0300

    1

--- WeWell/Services/TokenService.cs
+++ WeWell/Services/TokenService.cs
@@ -16,7 +16,7 @@ public class TokenService : ITokenService
             issuer: AuthOptions.ISSUER,
             audience: AuthOptions.AUDIENCE,
             claims: claims,
-            expires: DateTime.UtcNow.AddMinutes(5),
+            expires: DateTime.UtcNow.AddMinutes(1),
             signingCredentials: signinCredentials
         );
         var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
